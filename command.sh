2ddedfb7df9f0643dd3556e9f7a0c8569127c279
MrDoritos <ianmoore322@gmail.com>
2020-05-25 16:56:51 -0600

    dunno

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-g++ -I../console/ ../console/console.linux.cpp ../console/advancedConsole.cpp framebuffer.cpp box.cpp screen.cpp element.cpp console-ui.cpp -o console-ui -g -w -fpermissive -lpthread -lncurses
+g++ -I../console/ ../console/console.linux.cpp ../console/advancedConsole.cpp framebuffer.cpp box.cpp screen.cpp element.cpp console-ui.cpp -o console-ui -g -w -fpermissive -lpthread -lncursesw
